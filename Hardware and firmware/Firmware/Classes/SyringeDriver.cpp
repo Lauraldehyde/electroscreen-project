3fda7a4e41c9af2e3cce7a3d13703b35cfe55d84
Laura Moody <54102319+Lauraldehyde@users.noreply.github.com>
2020-07-14 17:15:34 +0100

    Create SyringeDriver.cpp

---
+++ Hardware and firmware/Firmware/Classes/SyringeDriver.cpp
@@ -0,0 +1,18 @@
+/*
+    SyringeDriver.h - Library for control of a singular electroscreen syringe driver
+    Created by Laura Moody, 14th July 2020
+    University of Portsmouth
+*/
+
+#include "Arduino.h"
+#include "SyringeDriver.h"
+
+SyringeDriver::SyringeDriver(int initialisationParas)
+{
+    //initialisation
+}
+
+void SyringeDriver::function()
+{
+    //write function
+}

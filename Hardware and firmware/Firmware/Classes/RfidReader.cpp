7bb58a45c629d8fff79bd534c5825223d8e417aa
Laura Moody <54102319+Lauraldehyde@users.noreply.github.com>
2020-08-06 18:34:23 +0100

    UPdates to RfidREader

--- Hardware and firmware/Firmware/Classes/RfidReader.cpp
+++ Hardware and firmware/Firmware/Classes/RfidReader.cpp
@@ -18,64 +18,112 @@
 #define SS_PIN 10;
 #define RST_PIN 9;
 
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::StatusCode status;
 MFRC522::MIFARE_Key key;
-int dataBlocks [6];
-String results [6];
+
 
 RfidReader::RfidReader()
 {
-    //initialisation of pins to connection
-    MFRC522 mfrc522(SS_PIN, RST_PIN);
-    SPI.begin();
-    for (byte i=0; i < 6; i++) key.keyByte[i] = 0xFF;
-    boolean a = 0;
+  int batchBlock =4, analysisBlock =5, cutoffBlock =6,highBlock =8, lowBlock =9, scanBlock =10, repeatBlock =12, expiryBlock =13, bioBlock = 14, fluidBlock = 16;
+  String batch = "", analysis = "", cutoff = "", high = "", low = "", scan = "", repeat = "", expiry = "", bio = "", fluid = "";
+  String descriptor[10] = {"batch", "analysis", "cutoff", "high", "low", "scan", "repeat", "expiry", "bio", "fluid"};
+  byte readBackBlock[18];
+  int blockInt [10] = {batchBlock, analysisBlock, cutoffBlock, highBlock, lowBlock, scanBlock, repeatBlock, expiryBlock, bioBlock, fluidBlock};
+  String parameterOut [10] = {batch,analysis,cutoff,high,low,scan,repeat,expiry, bio, fluid};
 
 }
 
-String RfidReader::readRfid(int block)
+String RfidReader::readRfid()
 {
-    //write function
+    bool mode = getMode();
+    getRfidData();
+    if(mode)
+    {
+      for (int i =0; i <10; i++){
+      char charArray[16];
+      String joinChar = "";
+      byteReadBlock(blockInt[i], readBackBlock);
+      for (int j=0; j<16; j++){
+        charArray[j] = char(readBackBlock[j]);
+      }
+      for (int u=0; u<16; u++){
+        joinChar = joinChar + charArray[u];
+        Serial.write(charArray[u]);
+      }
+      parameterOut[i] = joinChar;
+      Serial.flush();
+    }
+    }
 }
 
-int RfidReader::readBlock(int block){
-  byte len = 18;
-  byte buffer1[18];
-  char strOut[18];
-  int val;
+//METHODS for Serial communication
+bool RfidReader::getMode(){
+  String full;
+  int val = 0;
+  int fullInt = 0;
+  String junk = "";
+  int index;
+  while (Serial.available() == 0);
+  {
+    full = Serial.readStringUntil('\n');
+    while (Serial.available()> 0)
+    { junk = Serial.read(); }
+    index = full.indexOf("True");
+    if (index == -1){
+      return false;
+    }else {
+      return true;
+    }
+  }  
+}
+
+//METHODS FOR RFID READING
+//initialising RFID reader and reading
+void RfidReader::getRfidData(){
+  SPI.begin(); //SPI bus for RFID reader communication
+  mfrc522.PCD_Init(); //RFID reader initialised
+  for (byte i =0; i < 6; i++) key.keyByte[i] = 0xFF; //populating key with default password
+  boolean a = 0; 
+  while (a == 0){
+    boolean x = findCard();
+    a = x;
+  }
+}
+
+//Return True when a RFID is present
+boolean RfidReader::findCard(){
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if( ! mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+  Serial.println(F("Card Detected"));
+  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump details about the card
+  return 1;
+  
+}
+
+//Block reading method
+int RfidReader::byteReadBlock(int block, byte byteArray[]){
+  byte bufferSize = 18;
   int largestModulo4Number = block/4*4;
   byte trailerBlock = largestModulo4Number+3;
-  
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
+
+  //Check authentication status
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  if(status != MFRC522::STATUS_OK){
+    Serial.print("PCD_Authenticate() failed (read): ");
     Serial.println(mfrc522.GetStatusCodeName(status));
-    Serial.println("error in PCD_Authenticate code block");
-    return;
+    return 3;//return "3" as error message
   }
 
-  status = mfrc522.MIFARE_Read(block, buffer1, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
+  //Try to read a block
+  status = mfrc522.MIFARE_Read(block, byteArray, &bufferSize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
+  if (status != MFRC522::STATUS_OK){
+    Serial.print("MIFARE_read() failed: ");
     Serial.println(mfrc522.GetStatusCodeName(status));
-    Serial.println("Error in MIFARE_Read code block");
-    return;
+    return 4; //4 is returned as an error message
   }
-
-  for (int i = 0; i < 10; i++)
-  {
-      if (buffer1[i] > 44 && buffer1[i] < 58)
-        {
-          Serial.write(buffer1[i]);
-          strOut[i] = buffer1[i];
-          val = atoi(strOut);
-        } else if(buffer1[i] == NULL){
-          return val;
-        }
-  }
-  Serial.println("Never made NULL distinction");
-  Serial.println(strOut);
-  Serial.println(val);
-  return val; 
-  
 }
--- Hardware and firmware/Firmware/Classes/RfidReader.h
+++ Hardware and firmware/Firmware/Classes/RfidReader.h
@@ -11,14 +11,62 @@
 class RfidReader
 {
     public:
-        RfidReader(int connection); //update initiation to include all pins needed for control
-        String readRfid(int block);
-        int readBlock(int block);
-        int dataBlocks[6];
-        String results[6];
+        //methods
+        RfidReader(); //update initiation to include all pins needed for control
+        String readRfid();
+        bool getMode();
+        void getRfidData();
+        boolean findCard();
+        int byteReadBlock(int block, byte byteArray[]);
+        
+        //variables
+        MFRC522 mfrc522(SS_PIN, RST_PIN);
+        MFRC522::StatusCode status;
+        MFRC522::MIFARE_Key key;
+
+        int batchBlock;
+        int analysisBlock;
+        int cutoffBlock;
+        int highBlock;
+        int lowBlock;
+        int scanBlock;
+        int repeatBlock;
+        int expiryBlock;
+        int bioBlock;
+        int fluidBlock;
+        String batch;
+        String analysis;
+        String cutoff;
+        String high;
+        String low;
+        String scan;
+        String repeat;
+        String expiry;
+        String bio;
+        String fluid;
+        String descriptor[10];
+        byte readBackBlock[18];
+        int blockInt [10];
+        String parameterOut [10];
+
+        bool mode;
+        char charArray[16];
+        String joinChar = "";
+        String full;
+        int val;
+        int fullInt;
+        String junk;
+        int index;
+        boolean a;
+
+        byte bufferSize;
+        int largestModulo4Number;
+        byte trailerBlock;
+        byte status;
+  
+
     private:
         //wiring of pins to be put in here
-        int _pin;
 
 };
 

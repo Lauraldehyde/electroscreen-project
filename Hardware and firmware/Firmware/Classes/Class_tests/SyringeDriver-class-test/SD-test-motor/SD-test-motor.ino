1c8644df47835ab87a8a34e7517b4461d62d105f
Lauraldehyde <laura_moody@ymail.com>
2021-01-29 11:57:40 +0000

    class test update
    Class test updated to include switch limiters

--- Hardware and firmware/Firmware/Classes/Class_tests/SyringeDriver-class-test/SD-test-motor/SD-test-motor.ino
+++ Hardware and firmware/Firmware/Classes/Class_tests/SyringeDriver-class-test/SD-test-motor/SD-test-motor.ino
@@ -3,12 +3,12 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Initialising SyringeDriver");
-  SyringeDriver motor(2038, 8, 10, 9, 11,6,7);
+  SyringeDriver motor(2038, 8, 10, 9, 11, 6, 7);
   Serial.println("Setting speed");
   motor.setSpeed(5);
-  Serial.println("Moving forward 1000");
-  motor.moveSteps(4000);
-  Serial.println("Moving backward 1000");
+  Serial.println("Moving anticlockwise, pulling back, to fill 1000");
+  motor.moveSteps(1000);
+  Serial.println("Moving clockwise, pushing forward to  empty 1000");
   motor.moveSteps(-4000);  
 
 }
